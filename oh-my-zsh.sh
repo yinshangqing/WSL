b34f54bb1542e5311783b70a65a3fc76d26a132e
yinshangqing <yinshangqing@vip.qq.com>
2019-11-04 14:22:27 +0800

    (1) 更新插件

--- oh-my-zsh.sh
+++ oh-my-zsh.sh
@@ -13,10 +13,10 @@ vim ~/.zshrc
 # ZSH_THEME="lambda"                                                                            
 # ZSH_THEME="agnoster"
 # ZSH_THEME="robbyrussell"
-# plugins=(git z last-working-dir extract web-search osx rake rbenv ruby bundler dotenv)
+# plugins=(git z last-working-dir extract web-search osx rake rbenv ruby bundler dotenv zsh-syntax-highlighting zsh-autosuggestions zsh-completions zsh-history-substring-search)
 # unsetopt BG_NICE(如果报错，加上这一句)
-# git clone https://github.com/zsh-users/zsh-syntax-highlighting
-# git clone https://github.com/zsh-users/zsh-autosuggestions
-# git clone https://github.com/zsh-users/zsh-completions
-# git clone https://github.com/zsh-users/zsh-history-substring-search
-# source ~/oh-my-zsh/custom/plugins/-history-substring-search/zsh-history-substring-search.zsh
+# git clone https://github.com/zsh-users/zsh-syntax-highlighting $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+# git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+# git clone https://github.com/zsh-users/zsh-completions $ZSH_CUSTOM/plugins/zsh-completions
+# git clone https://github.com/zsh-users/zsh-history-substring-search $ZSH_CUSTOM/plugins/zsh-history-substring-search
+ 
